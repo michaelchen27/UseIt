b40b2b8e8af84bfcf72205977ba073176a703c44
Michael <michael.chen2701@gmail.com>
2021-03-30 18:44:14 +0700

    slider on landing page

--- app/src/main/java/umn/useit/MainActivity.java
+++ app/src/main/java/umn/useit/MainActivity.java
@@ -1,14 +1,22 @@
 package umn.useit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+    private SliderAdapter sliderAdapter;
+    private TextView[] mDots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,23 +26,62 @@ public class MainActivity extends AppCompatActivity {
         Button btn_sign_in = (Button) findViewById(R.id.btn_sign_in);
         Button btn_sign_up = (Button) findViewById(R.id.btn_sign_up);
 
-        // SIGN IN Button Click
-        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(this);
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() { // SIGN IN Button Click
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SignInActivity.class));
             }
         });
 
-        // SIGN UP Button Click
-        btn_sign_up.setOnClickListener(new View.OnClickListener() {
+
+        btn_sign_up.setOnClickListener(new View.OnClickListener() { // SIGN UP Button Click
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SignUpActivity.class));
             }
         });
 
+        addDotsIndicator(0);
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
+    } //onCreate()
+
+
+    public void addDotsIndicator(int position) { // Dots Indicator for Slider
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+        for(int i = 0; i < mDots.length; i++) {
+
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.light_grey));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0) {
+            mDots[position].setTextColor(getResources().getColor(R.color.teal));
+        }
     }
 
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() { //Slide change listener
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+        @Override
+        public void onPageSelected(int position) { addDotsIndicator(position);}
+
+        @Override
+        public void onPageScrollStateChanged(int state) {}
+    };
 
 }
---
+++ app/src/main/java/umn/useit/SliderAdapter.java
@@ -0,0 +1,77 @@
+package umn.useit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    //Arrays
+    public int[] slide_images =  {
+            R.drawable.it_support,
+            R.drawable.clock,
+            R.drawable.phone
+    };
+
+    public String[] slide_headings = {
+            "Welcome to UseIt",
+            "Always online 24/7",
+            "Multiple Channel Support"
+    };
+
+    public String[] slide_descs = {
+            "Solve your IT related problems with the help of experts",
+            "Get help anytime",
+            "Get help anywhere"
+    };
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (ConstraintLayout) object;
+    }
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageVIew = (ImageView) view.findViewById(R.id.slide_image);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slide_heading);
+        TextView slideDescription = (TextView) view.findViewById(R.id.slide_desc);
+
+        slideImageVIew.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slide_descs[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
+
+}
