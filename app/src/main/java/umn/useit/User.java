53bcc12b7cb4a012ce7874b8d7aa9600ba5d621f
Michael <michael.chen2701@gmail.com>
2021-04-06 01:19:34 +0700

    store user auth uid to realtime db

---
+++ app/src/main/java/umn/useit/User.java
@@ -0,0 +1,27 @@
+package umn.useit;
+
+public class User {
+
+    private String firstname, lastname, email;
+
+    User(){}
+
+    public User(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
