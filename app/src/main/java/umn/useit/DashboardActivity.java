5cddcf0f407ca2a3c348293750ac1ca9736bb30d
Michael <michael.chen2701@gmail.com>
2021-04-06 13:08:06 +0700

    animation bottomnav

--- app/src/main/java/umn/useit/DashboardActivity.java
+++ app/src/main/java/umn/useit/DashboardActivity.java
@@ -2,20 +2,26 @@ package umn.useit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    TextView welcome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        welcome = findViewById(R.id.welcome);
 
         //Bottom Nav
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
@@ -23,28 +29,41 @@ public class DashboardActivity extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    switch(menuItem.getItemId()) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                switch(menuItem.getItemId()) {
                         case R.id.nav_home:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                            transaction.setCustomAnimations(
+                                    R.anim.enter_left_to_right, //enter
+                                    R.anim.exit_left_to_right); //exit
+                            changeFragment(transaction, new HomeFragment(), "HOME_FRAGMENT");
+
                             break;
                         case R.id.nav_ask:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AskFragment()).commit();
+                            transaction.setCustomAnimations(
+                                    R.anim.enter_down_to_up,
+                                    R.anim.exit_down_to_up);
+                            changeFragment(transaction, new AskFragment(), "ASK_FRAGMENT");
+
                             break;
                         case R.id.nav_profile:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                            transaction.setCustomAnimations(
+                                    R.anim.enter_right_to_left,
+                                    R.anim.exit_right_to_left);
+                            changeFragment(transaction, new ProfileFragment(), "PROFILE_FRAGMENT");
+
                             break;
                     }
                 return true;
             }
         }); //Bottom Nav
-
     } //onCreate()
 
-//    public void changeFragment(int layout, Fragment newFrag) {
-//        getSupportFragmentManager()
-//                .beginTransaction()
-//                .replace(layout, newFrag)
-//                .commit();
-//    }
+
+    public void changeFragment(FragmentTransaction transaction, Fragment f, String tag) {
+        transaction
+        .replace(R.id.fragment_container, f, tag)
+        .commit();
+    }
 
 }
